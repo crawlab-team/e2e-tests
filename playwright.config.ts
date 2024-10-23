c6c69d827f7dbe14d584ee3e0029892e68bd4719
Marvin Zhang <tikazyq@163.com>
2024-10-23 13:58:41 +0800

    chore: updated config

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,8 +38,11 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
 
-    /* Disable headless mode */
+    /* Configure headless mode */
     headless,
+
+    /* Take screenshot on failure */
+    screenshot: 'only-on-failure',
   },
 
   /* Configure timeout */
